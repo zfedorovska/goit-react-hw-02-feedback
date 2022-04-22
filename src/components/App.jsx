80fb9805fd95490874c40c62321552709ea24efc
zfedorovska <43138422+zfedorovska@users.noreply.github.com>
2022-04-22 16:24:17 +0300

    removed phoneBook logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,9 @@
 import { Component } from 'react';
-import { nanoid } from 'nanoid';
 import Section from './Statistics/Section';
 import StatisticsClass from 'components/Statistics/StatisticsClass';
-import ContactForm from './PhoneBook/ContactForm';
-import Filter from './PhoneBook/Filter';
-import ContactList from './PhoneBook/ContactList';
 
 export default class App extends Component {
-  state = {
-    contacts: [ {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }],
-    filter: ''
-  }
-
-  addContact = data => {
-    const {contacts} = this.state
-    let isContactNameFound = contacts.find(contact => (contact.name === data.name))
-    if (isContactNameFound) {
-      alert(`${data.name} is already in contacts`);
-    }
-    else {
-       const contact = {
-      id: nanoid(),
-      name: data.name,
-      number: data.number
-    };
-    this.setState(({contacts}) => ({
-      contacts: [contact, ...contacts]
-    }))
-    }  
-  };
-
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId)
-    }))
-  };
-
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
-  }
-
-  getVisibleContacts = () => {
-    const { contacts, filter } = this.state;
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
-  }
-
   render() {
-    const { contacts, filter } = this.state;
-    const visibleContacts = this.getVisibleContacts();
     return (
       <div>
          <StatisticsClass/>
@@ -59,5 +11,3 @@ export default class App extends Component {
     );
   }
 }
-
-
